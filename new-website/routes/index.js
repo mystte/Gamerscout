02d7dfbecf34f9d9d146125a26b14e7741e316d7
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2017-08-18 14:12:24 -0700

    front-end Wireframe

--- new-website/routes/index.js
+++ new-website/routes/index.js
@@ -6,5 +6,13 @@ router.get('/', function(req, res, next) {
   // Call to API HERE
   res.render('index', { title: 'Repflame'});
 });
+//search route just for ui testing purposes
+router.get('/search', function(req, res, next) {
+	res.render('pages/search-page', { title: 'Repflame'});
+});
+//review route for ui testing purposes
+router.get('/player', function(req, res, next) {
+	res.render('pages/player_page', { title: 'Repflame'});
+});
 
 module.exports = router;
