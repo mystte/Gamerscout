f841f50c1ac729bedae4883109015790e9ab187f
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2017-11-09 15:17:36 -0800

    added search page route

--- website/routes/index.js
+++ website/routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Call to API HERE
-  res.render('index', { title: 'Repflame'});
+  res.render('index', { title: 'repGG'});
+});
+/* get search page */
+router.get('/search', function(req, res, next){
+	res.render('./pages/search_page', { title: 'repGG'});
 });
 
 module.exports = router;
