6d4582006b03ff76edf66bf344cc27ee21cc09d4
mystte <stephane.rose60@gmail.com>
2017-12-10 23:57:08 -0500

    Plug signup login route

--- website/public/js/ui/login/signup_signin_modal.js
+++ website/public/js/ui/login/signup_signin_modal.js
@@ -22,14 +22,53 @@ $(document).ready(function() {
     }
   }
 
+  var submit = function(mode) {
+    console.log("Submit");
+    var data = {};
+    var url = "";
+    if (mode === MODE.SIGNIN) {
+      url = "/login";
+      data = {
+        "username" : $('.signup-mode .email').eq(0).text(),
+        "password" : $('.signup-mode .password').eq(0).text(),
+        "email" : $('.signup-mode .email').eq(0).text()
+      }
+    } else {
+      url = "/signup";
+      data = {
+        "username" : $('.signin-mode .email').eq(0).text(),
+        "password" : $('.signin-mode .password').eq(0).text(),
+        "email" : $('.signin-mode .email').eq(0).text()
+      }
+    }
+    $.ajax({
+      contentType: "application/json",
+      type: 'POST',
+      data: JSON.stringify(data),
+      url: url,
+      success: function(result) {
+      },
+      error: function(error) {
+      }
+    });
+  }
+
   // jQuery hook
   $(".js-signin-mode").click(function() {
     toggleSigninSignup(MODE.SIGNIN);
   });
 
   $(".js-signup-mode").click(function() {
-    toggleSigninSignup(MODE.signup);
+    toggleSigninSignup(MODE.SIGNUP);
   });
+
+  $(".js-signin").click(function() {
+    submit(MODE.SIGNIN);
+  });  
+
+  $(".js-signup").click(function() {
+    submit(MODE.SIGNUP);
+  });  
 
   var root = $("#signup-signin-modal");
   if (root.length) {
--- website/routes/index.js
+++ website/routes/index.js
@@ -2,12 +2,47 @@ var express = require('express');
 var router = express.Router();
 var requests = require('../utils/requests.js');
 var constants = require('../utils/constants.js');
+var Q = require('q');
+var config = require('../config/common.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Call to API HERE
   var reviews = requests.do_get_request(`${constants.API_BASE_URL}/getRandomPlayers/3`).then(function(result) {
     res.render('index', { title: 'Repflame', gamers: result.body.gamers });
+  });
+});
+
+router.post('/login', function(req, res, next) {
+  var uri = config.api.url + ":" + config.api.port + "/api/1/users/login";
+  var data = {
+    email : req.body.email ? req.body.email : null,
+    password : req.body.password ? req.body.password : null
+  };
+  Q().then(function() {
+    return requests.do_post_request(uri, data);
+  }).then(function(result) {
+    res.status(201).json(result);
+  }).catch(function(reason) {
+    console.log(reason);
+    res.status(500).json({err : "Internal Server Error"});
+  });
+});
+
+router.post('/signup', function(req, res, next) {
+  var uri = config.api.url + ":" + config.api.port + "/api/1/users/signup";
+  var data = {
+    email : req.body.email ? req.body.email : null,
+    password : req.body.password ? req.body.password : null,
+    username : req.body.email ? req.body.email : null,
+  };
+  Q().then(function() {
+    return requests.do_post_request(uri, data);
+  }).then(function(result) {
+    res.status(201).json(result);
+  }).catch(function(reason) {
+    console.log(reason);
+    res.status(500).json({err : "Internal Server Error"});
   });
 });
 
