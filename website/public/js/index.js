870b6da0080d3faca46e37ea0160f7c499421cc0
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2018-02-05 13:33:48 -0800

    Fixed route, no longer gets lol_region_short not defined error

--- website/public/js/index.js
+++ website/public/js/index.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
     	//Always searches league: update when more games added
     	var region = $('#region-selection').val();
     	var gamertag = $('#gamertag-to-search').val();
-  		var profile_url = window.location.href + "profile/lol/" + region + "1/" + gamertag; 
+  		var profile_url = "/profile/lol/" + region + "1/" + gamertag; 
   		window.location.href = profile_url;
-});
+	});
+	$( "#search-icon-nav").click(function(){
+		var region = $('#region-selection-nav').val();
+		var gamertag = $('#search-nav').val();
+		var profile_url = "/profile/lol/" + region + "1/" + gamertag; 
+  		window.location.href = profile_url;
+	});
   }
 });
