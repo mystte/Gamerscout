f62b31f21db0ddc41a661d0d285275523ec65fd6
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2018-02-22 14:29:37 -0800

    Link player cards to profile

--- website/public/js/index.js
+++ website/public/js/index.js
@@ -27,9 +27,33 @@ $(document).ready(function() {
 		});
 
     $(".featured-list").on('click', '#recent-view-item-container', function(){
-      var a = $(this).attr("id");
-      console.log(a);
+      var a = $(this).text();
+      var info = a.split("\n");
+      $.each(info, function (id, val) {
+        info[id] = $.trim(val);
+      });
+      var finalInfo = info.filter(function(v){return v!==''});
+      shortRegion = toShortNotation(finalInfo[0]);
+      var profileUrl = "profile/lol/" + shortRegion + "/" + finalInfo[1];
+      window.location.href = profileUrl;
     });
+
+    function toShortNotation(region){
+      var regions = {
+        "na1" : "North America",
+        "br1" : "Brazil",
+        "eune1" : "Europe North & East",
+        "euw1" : "Europe West",
+        "kr1" : "Korea",
+        "lan1" : "Latin America North",
+        "las1" : "Latin America South",
+        "oce1" : "Oceania",
+        "ru1" : "Russia",
+        "tr1" : "Turkey"
+      } 
+      return Object.keys(regions).filter(function(key) {return regions[key] === region})[0];
+
+    }
 		// detect enter keypress
 		$(document).keypress(function (e) {
 			var keycode = (e.keyCode ? e.keyCode : e.which);
@@ -38,5 +62,6 @@ $(document).ready(function() {
 				search_lol_player()
 			}
 		});
+
 	}
 });
