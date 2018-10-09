aeee63d3ae97cf77d13bc583e1c4f42d72415392
Stéphane ROSE <srose@turbulent.ca>
2018-10-08 22:45:53 -0300

    Run production server on 80 port so it can shows urls

--- api/app.js
+++ api/app.js
@@ -37,7 +37,6 @@ mongoose.connect(mongoConnStr, mongoOptions).then(() => {
   console.log('Successfully connected to MongoDB:' + config.project_name + ' on port ' + config.mongodb_port);
   console.log('Gamerscout is running in ' + app.get('env') + ' environment');
 }, (err) => {
-  console.log("##### ERR", err);
   throw err;
 });
 
